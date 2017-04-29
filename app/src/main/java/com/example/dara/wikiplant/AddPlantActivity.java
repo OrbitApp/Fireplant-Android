6301f7703ce5ceb8f11aeb77b1523d36d767c2a0
Liew Jun Tung <pandawarrior91@gmail.com>
2017-04-29 15:02:23 +0800

    WIP AddPlantActivity

--- app/src/main/java/com/example/dara/wikiplant/AddPlantActivity.java
+++ app/src/main/java/com/example/dara/wikiplant/AddPlantActivity.java
@@ -1,19 +1,23 @@
 package com.example.dara.wikiplant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddPlantActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @BindView(R.id.imageView)
     ImageView imageView;
     @BindView(R.id.editText_name)
@@ -33,16 +37,24 @@ public class AddPlantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
         ButterKnife.bind(this);
-
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-            }
-        });
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mDatabase = database.getReference("plants");
 
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @OnClick(R.id.button_add)
+    public void onViewClicked() {
+        String name = editTextName.getText().toString();
+        String genus = editTextGenus.getText().toString();
+        String taxonomy = editTextTaxonomy.getText().toString();
+        String description = editTextDescription.getText().toString();
+        mDatabase.push().setValue(new Plant(name, "pending", genus, taxonomy, description));
+    }
 }
