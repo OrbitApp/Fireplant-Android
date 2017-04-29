3fee832169d14e45244ee65c200aad536c89ba22
Dara <tumenbayevadara@gmail.com>
2017-04-30 02:04:39 +0800

    WIP, plant

--- app/src/main/java/com/example/dara/wikiplant/ListOfPlantsActivity.java
+++ app/src/main/java/com/example/dara/wikiplant/ListOfPlantsActivity.java
@@ -1,15 +1,79 @@
 package com.example.dara.wikiplant;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ListOfPlantsActivity extends AppCompatActivity {
 
-    private Button listOfPlants;
+    @BindView(R.id.RecyclerView)
+    android.support.v7.widget.RecyclerView RecyclerView;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    List<PlantClass> listData;
+    private ItemPlantAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_plants);
+        ButterKnife.bind(this);
+        initRecyclerview();
+        RecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        RecyclerView.setLayoutManager(llm);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("plants");
+        final PlantClass fire = new PlantClass();
+        listData = new ArrayList<PlantClass>();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+
+                    PlantClass value = dataSnapshot1.getValue(PlantClass.class);
+
+                    String name = value.getName();
+                    String description = value.getDescription();
+                    fire.setName(name);
+                    fire.setDescription(description);
+                    listData.add(fire);
+                    System.out.println(fire.getName());
+                    System.out.println(fire.getDescription());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        System.out.println(listData);
+        adapter = new ItemPlantAdapter(listData, this);
+        RecyclerView.setAdapter(adapter);
+
+    }
+
+    private void initRecyclerview() {
+
     }
 }
--- app/src/main/java/com/example/dara/wikiplant/MainMenuActivity.java
+++ app/src/main/java/com/example/dara/wikiplant/MainMenuActivity.java
@@ -1,14 +1,42 @@
 package com.example.dara.wikiplant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_list)
+    Button buttonList;
+    @BindView(R.id.button_scan)
+    Button buttonScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.button_list, R.id.button_scan})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button_list:
+                Intent intent = new Intent(MainMenuActivity.this, ListOfPlantsActivity.class);
+                startActivity(intent);
+
+
+                break;
+            case R.id.button_scan:
+
+                break;
+        }
     }
 }
